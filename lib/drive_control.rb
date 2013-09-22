9cdb406905a50c7edef80c19b2a4268830f705b8
Daniel Evans <evans.daniel.n@gmail.com>
2013-09-21 22:49:03 -0600

    Add rudimentary roaming logic and clean up driving a bit

--- lib/drive_control.rb
+++ lib/drive_control.rb
@@ -1,49 +1,49 @@
 module DriveControl
-  def location_heuristic(target, wall)
-    heuristic = distance(robot, wall)
-    if target
-      heuristic += distance(target, wall)
-    end
-    heuristic
+  def location_heuristic(wall)
+    distance(robot, wall)
   end
 
-  def find_drive_target(target)
+  def find_drive_target
     if battle.board.walls.length > 0
       target_wall = battle.board.walls.reduce(nil) do |memo, wall|
-        if memo.nil? || location_heuristic(target, wall) < location_heuristic(target, memo)
+        if memo.nil? || location_heuristic(wall) < location_heuristic(memo)
           wall
         else
           memo
         end
       end
-      target = board.line_of_sight(target_wall, 360 - robot.direction_to(target_wall)).first
-      target unless robot.located_at?(target)
+      battle.board.line_of_sight(target_wall, board.direction_to(target_wall, robot)).first
     end
   end
 
   def drive_control_turn
-    @drive_target_history ||= []
     result = nil
+    @drive_target_history ||= []
     rt = Benchmark.realtime do
-      target = choose_target
-      if !@drive_target
-        @drive_target = find_drive_target(target)
-        if @drive_target_history.include?(@drive_target) || @drive_target.nil?
-          @drive_target = nil
-        else
-          @drive_target_history << @drive_target
-        end
+      @drive_target = nil if @drive_target && @drive_target.located_at?(robot)
+
+      unless @drive_target
+        @drive_target = find_drive_target
+        @drive_target = nil if @drive_target && @drive_target_history.include?([@drive_target.x,@drive_target.y])
       end
 
-      if @drive_target && @drive_target.located_at?(robot)
-        @drive_target = nil
-      end
+      @drive_target = nil if @drive_target && @drive_target.located_at?(robot)
 
       if @drive_target
         result = move_towards!(@drive_target)
-      elsif target
-        result = move_towards!(target)
+      else
+        los = battle.board.line_of_sight(robot, 180)
+        if los && los.length > 0 && wall_at?(los.first)
+          @drive_target = los.last
+        else
+          @drive_target = (battle.board.line_of_sight(robot, 0) || []).last
+        end
+        if @drive_target
+          result = move_towards!(@drive_target)
+        end
       end
+
+      @drive_target_history << [@drive_target.x, @drive_target.y] if @drive_target
     end
 
     log "Drive Control Turn handled in #{rt} with result #{result};"
